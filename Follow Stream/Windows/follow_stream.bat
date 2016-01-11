2d3d7eafc57e2d4e3effb5a5d65597ea942f48e8
Brian Maloney <bmmaloney97@gmail.com>
2016-01-10 21:51:51 -0600

    Fixed file not found issue and parsing speed

--- Follow Stream/Windows/follow_stream.bat
+++ Follow Stream/Windows/follow_stream.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal enabledelayedexpansion
 set out=%PROCDOTPLUGIN_ResultTXT%
-set tcpflow=<path to tcpflow> -T %%N.%%A.%%a-%%B.%%b%%V%%v%%C%%c
+set tcpflow=<path to tcpflow> -T %%N_%%A-%%B
 set temp=%LOCALAPPDATA%\temp\tcpflow_out
 set IP=%PROCDOTPLUGIN_CurrentNode_Details_IP-Address%
 
@@ -9,7 +9,7 @@ set IP=%PROCDOTPLUGIN_CurrentNode_Details_IP-Address%
 
 call :parse %IP%
 set new_IP=%one:~-3%.%two:~-3%.%three:~-3%.%four:~-3%
-forfiles /p %temp% /m * /c "cmd /c echo. >> @file | echo. >> @file"
+forfiles /p %temp% /m *%new_IP%* /c "cmd /c echo. >> @file | echo. >> @file"
 type %temp%\*%new_IP%* > %temp%\PBTEMP
 more %temp%\PBTEMP > %out%
 rmdir /S /Q %temp%
@@ -17,9 +17,9 @@ rmdir /S /Q %temp%
 :parse
 set list=%1
 FOR /f "tokens=1,2,3,4 delims=." %%a IN ("%list%") DO (
-  set one=0%%a
-  set two=0%%b
-  set three=0%%c
-  set four=0%%d
+  set one=00%%a
+  set two=00%%b
+  set three=00%%c
+  set four=00%%d
 )
 exit /b
