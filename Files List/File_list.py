ebb567dceaf752b98d64414abfaa391891125024
Brian Maloney <Beercow@users.noreply.github.com>
2016-06-10 10:48:58 -0500

    Fixed file path not showing up
    File path was not showing up on some entries

--- Files List/File_list.py
+++ Files List/File_list.py
@@ -6,7 +6,7 @@ def main():
     
     data = os.getenv('PROCDOTPLUGIN_GraphFileDot')
     out = os.getenv('PROCDOTPLUGIN_ResultCSV')
-    outfile = open(out, 'wb')
+    outfile = open(out, 'w')
     outfile.write ('"File Path","Exists"\n')
     outfile.write ('"*","*"\n')
     with open(data) as f:
@@ -18,11 +18,9 @@ def main():
                     f2 = (','.join(line[3:4]))
                     outfile.write('{{color:purple}}' + '"' + f1[5:] + ' -> ' + f2[5:] + '","rename"\n')
                 elif 'fontcolor = red' in line:
-                    line = line.strip().split('=')
-                    line = (','.join(line[1:2]) + '\n')
-                    line = line.strip().split(',')
-                    line = (','.join(line[0:1]) + ',"No"\n') 
-                    outfile.write('{{color:red}}' + line)  
+                    line = line.strip().split('"')
+                    line = (','.join(line[1:2]))
+                    outfile.write('{{color:red}}' + '"' + line[5:] + '","No"\n')  
                 else:
                     line = line.strip().split('"')
                     line = (','.join(line[1:2]))
