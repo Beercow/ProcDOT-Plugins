3e59eb4285c36f8db491fdcdb99c9ae84b206fb0
Brian Maloney <Beercow@users.noreply.github.com>
2017-08-24 12:10:50 -0500

    Update save_as_png.bat
    Added gui to resize image.

--- save_as_png/save_as_png.bat
+++ save_as_png/save_as_png.bat
@@ -2,8 +2,10 @@
 #!/usr/bin/env python
 
 from Tkinter import *
+import Tkconstants
 import tkFileDialog
 import os
+import sys
 import subprocess as sub
 import base64
 
@@ -19,15 +21,37 @@ def icon(tempFile):
     iconfile.close()
     return tempFile
 
-def gui(tempFile):
-    root = Tk()
-    root.withdraw() # hide root
-    root.wm_iconbitmap(tempFile)
-    name = tkFileDialog.asksaveasfilename(filetypes=[('PNG','*.png')], title='Save Graph as PNG', defaultextension='.png')
+def asksaveasfilename(val):
+    val = float(val.get()) / float(100)
+    val += float(1)
+    val = val*96
+    dpi = '-Gdpi=' + str(val)
+    name = tkFileDialog.asksaveasfilename(filetypes=[('PNG','*.png')], title='Export Canvas to PNG', defaultextension='.png')
     if name:
         with open(name, 'w') as f:
-            p = sub.Popen(['dot', (os.getenv('PROCDOTPLUGIN_GraphFileDot')), '-Tpng', '-Gdpi=300', '-o', (name) ], stdout=sub.PIPE, stderr=sub.PIPE)
+            p = sub.Popen(['dot', (os.getenv('PROCDOTPLUGIN_GraphFileDot')), '-Tpng', (dpi), '-o', (name) ], stdout=sub.PIPE, stderr=sub.PIPE)
             p.wait()
+    sys.exit(0)
+
+def print_value(val, root):
+    root.quit()
+
+def on_closing(root):
+    root.destroy()
+    sys.exit(0)
+    
+def gui(tempFile):
+    
+    root = Tk()
+    root.wm_iconbitmap(tempFile)
+    root.title('Export Canvas to PNG')
+    val = Scale(root, from_=0, to=200, length=300, label='Increase Canvas Size %', tickinterval=20, orient=HORIZONTAL)
+    val.pack()    
+    button_opt = {'padx': 5, 'pady': 5}
+    Button(root, text='Save PNG', command=lambda:asksaveasfilename(val)).pack(**button_opt)
+    root.bind("<Unmap>", lambda e: root.deiconify())
+    root.protocol("WM_DELETE_WINDOW", lambda:on_closing(root))
+    root.mainloop()
 
 def main():
     tempFile = None
