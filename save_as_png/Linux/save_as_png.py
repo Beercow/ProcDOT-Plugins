e53c42551c0fd6f527d69149bebdefba91381631
Brian Maloney <Beercow@users.noreply.github.com>
2017-12-15 06:50:42 -0600

    Update save_as_png.py

--- save_as_png/Linux/save_as_png.py
+++ save_as_png/Linux/save_as_png.py
@@ -8,18 +8,6 @@ import sys
 import subprocess as sub
 import base64
 
-def icon(tempFile):
-    temp = os.getenv('LOCALAPPDATA')
-    icon = \
-    """ AAABAAEAECAAAAEAIABoBAAAFgAAACgAAAAQAAAAIAAAAAEAIAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AAAAAA4AAAh/AAAVxAAACfIAAAnyAAAVxAAACH8AAAAO////AP///wD///8A////AP///wD///8A////AAAAAEIAAA3pAABx9wAAwP8AAO//AADv/wAAwP8AAHH3AAAN6QAAAEL///8A////AP///wD///8A////AAAAAEIAABfzAADL/wAA//8AAP//AAD//wAA//8AAP//AAD//wAAy/8AABfzAAAAQv///wD///8A////AAAAAA4AAA3pAADL/wAA//8AAP//AAD//wAA//8AAP//AAD//wAA//8AAP//AADL/wAADekAAAAO////AP///wAAAAh/AABx9wAA//8AAP//AAD//wAA//8AAP//AAD//wAA//8AAP//AAD//wAA//8AAHH3AAAIf////wD///8AAAAVxAAAwP8AAP//AAD//wAA//8AAP//AAD//wAA//8AAP//AAD//wAA//8AAP//AADA/wAAFcT///8A////AAAACfIAAO//AAD//wAA//8AAP//AAD//wAA//8AAP//AAD//wAA//8AAP//AAD//wAA7/8AAAny////AP///wAAAAnyAADv/wAA//8AAP//AAD//wAA//8AAP//AAD//wAA//8AAP//AAD//wAA//8AAO//AAAJ8v///wD///8AAAAVxAAAwP8AAP//AAD//wAA//8AAP//AAD//wAA//8AAP//AAD//wAA//8AAP//AADA/wAAFcT///8A////AAAACH8AAHH3AAD//wAA//8AAP//AAD//wAA//8AAP//AAD//wAA//8AAP//AAD//wAAcfcAAAh/////AP///wAAAAAOAAAN6QAAy/8AAP//AAD//wAA//8AAP//AAD//wAA//8AAP//AAD//wAAy/8AAA3pAAAADv///wD///8A////AAAAAEIAABfzAADL/wAA//8AAP//AAD//wAA//8AAP//AAD//wAAy/8AABfzAAAAQv///wD///8A////AP///wD///8AAAAAQgAADekAAHH3AADA/wAA7/8AAO//AADA/wAAcfcAAA3pAAAAQv///wD///8A////AP///wD///8A////AP///wAAAAAOAAAIfwAAFcQAAAnyAAAJ8gAAFcQAAAh/AAAADv///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A//8AAPw/AADwDwAA4AcAAMADAADAAwAAgAEAAIABAACAAQAAgAEAAMADAADAAwAA4AcAAPAPAAD8PwAA//8AAA==
-    """
-    icondata= base64.b64decode(icon)
-    tempFile= temp+"icon.ico"
-    iconfile= open(tempFile,"wb")
-    iconfile.write(icondata)
-    iconfile.close()
-    return tempFile
-
 def asksaveasfilename(val):
     val = float(val.get()) / float(100)
     val += float(1)
@@ -41,10 +29,9 @@ def on_closing(root):
     root.destroy()
     sys.exit(0)
     
-def gui(tempFile):
+def gui():
     
     root = Tk()
-    root.wm_iconbitmap(tempFile)
     root.title('Export Canvas to PNG')
     val = Scale(root, from_=0, to=200, length=300, label='Increase Canvas Size %', tickinterval=20, orient=HORIZONTAL)
     val.pack()    
@@ -55,10 +42,8 @@ def gui(tempFile):
     root.mainloop()
 
 def main():
-    tempFile = None
-    tempFile = icon(tempFile)
-    gui(tempFile)
-    os.remove(tempFile)
+
+    gui()
 
 if __name__ == '__main__':
     main()
