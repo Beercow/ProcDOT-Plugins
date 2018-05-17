04e80e3f68c7b9cb4e7bab1577efafd1a9368e27
Brian Maloney <Beercow@users.noreply.github.com>
2018-05-17 03:41:39 +0000

    Added GeoIP

---
+++ GeoIP/Linux/GeoIP.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+import os
+import pygeoip
+import sys
+import urllib
+import gzip
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+out = os.getenv('PROCDOTPLUGIN_ResultTXT')
+
+def database(url,dbgz,db):
+    geo = urllib.URLopener()
+    try:
+        geo.retrieve(url, dbgz)
+        with gzip.open(dbgz, 'rb') as infile:
+            with open(db, 'wb') as outfile:
+                for line in infile:
+                    outfile.write(line)
+        os.remove(dbgz)
+    except Exception as e:
+        open(out, 'wb').write(str(e))
+        sys.exit(0)
+
+if os.path.isfile('GeoLiteCity.dat'):
+    pass
+else:
+    database('http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz', 'GeoLiteCity.dat.gz', 'GeoLiteCity.dat')
+
+if os.path.isfile('GeoIPASNum.dat'):
+    pass
+else:
+    database('http://download.maxmind.com/download/geoip/database/asnum/GeoIPASNum.dat.gz', 'GeoIPASNum.dat.gz', 'GeoIPASNum.dat')
+       
+tfolder =  os.listdir(os.getenv('PROCDOTPLUGIN_TempFolder'))
+details = open(os.getenv('PROCDOTPLUGIN_GraphFileDetails'),'rb').readlines()
+n = open(os.getenv('PROCDOTPLUGIN_GraphFileDetails'),'w')
+
+for num, line in enumerate(details,1):
+    if 'IP-Address' in line:
+        query = pygeoip.GeoIP('GeoLiteCity.dat')
+        asn = pygeoip.GeoIP('GeoIPASNum.dat')
+        x = [x.strip() for x in line.split(' ')][2]
+        try:
+            results = query.record_by_addr(x)
+            asn_info = asn.asn_by_addr(x)
+            for key, val in results.items():
+                details.insert(num,str(key) + ' = ' + str(val) + '\n')
+
+            try:
+                details.insert(num,'asn = '+asn_info+'\n')
+            except:
+                pass
+        except:
+            pass
+details = "".join(details)
+n.write(details)
+n.close()
+sys.exit(0)
---
+++ GeoIP/README.md
@@ -0,0 +1,10 @@
+# GeoIP
+
+## Project source can be downloaded from
+
+## Author & Contributor List
+Brian Maloney
+
+## Overview
+This plugin adds geographical information to server nodes. GeoIP is an event hadler plugin so you will not see anything added to the graph or plugin menus. It is designed to run after the refresh button is clicked. The first run of GeoIP, you might have a slight delay because teh plugin needs to download the Maxmind database.
+
